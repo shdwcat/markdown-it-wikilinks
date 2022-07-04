9a1443f7ef94ec4a3925733b88e78b8b8aa93121
Quetzy <15136382+shdwcat@users.noreply.github.com>
2022-07-04 04:03:58 -0700

    Add support for headings in links e.g. [[File#Heading]] * works with description|file and file|description e.g. [[File#Heading|Show this instead]] * Supports heading only links like `[[#Heading]]`

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ module.exports = (options) => {
 
   options = extend(true, defaults, options)
 
+  const headingRegex = /([^#]+)?#?([^#]+)?/
+
   function isAbsolute(pageName) {
     return options.makeAllLinksAbsolute || pageName.charCodeAt(0) === 0x2F/* / */
   }
@@ -49,44 +51,70 @@ module.exports = (options) => {
       let href = ''
       let htmlAttrs = []
       let htmlAttrsString = ''
+
       const isSplit = !!match[2]
       if (isSplit) {
         if (options.description_then_file) {
           label = match[1]
-          pageName = match[2].replace(/^./, '') // Remove separator
+          pageName = match[2].replace(options.separator, '')
         } else {
-          label = match[2].replace(/^./, '') // Remove separator
+          label = match[2].replace(options.separator, '')
           pageName = match[1]
         }
-        
       }
       else {
         label = match[1]
         pageName = options.generatePageNameFromLabel(label)
       }
 
+      let headingMatch = headingRegex.exec(pageName)
+      pageName = headingMatch[1] ?? ''
+      let heading = headingMatch[2]?.split(/\s+/).join('-')
+
+      // e.g just '#HeadingName'
+      let isHeadingLink = heading && !pageName
+
+      // fixup label when there's a heading but no explicit description
+      if (heading && !isSplit) {
+        label = pageName || heading
+      }
+
       label = options.postProcessLabel(label)
       pageName = options.postProcessPageName(pageName)
 
       // make sure none of the values are empty
-      if (!label || !pageName) {
+      if (!label || (!pageName && pageName !== '')) {
         return match.input
       }
 
-      let uriSuffix = options.uriSuffix
-      if (!uriSuffix) {
-        var currentFileName = document.fsPath;
-        var currentExtension = currentFileName.split('.').pop()
-        uriSuffix = "." + currentExtension
-      }
-
-      if (isAbsolute(pageName)) {
-        pageName = removeInitialSlashes(pageName)
-        href = options.baseURL + pageName + uriSuffix
+      if (isHeadingLink) {
+        // in this case the href is just the link content, e.g. '#HeadingName'
+        href = "#" + heading.toLowerCase().split(/\s+/).join('-')
       }
       else {
-        href = options.relativeBaseURL + pageName + uriSuffix
+
+        // if a uriSuffix isn't provided, default to whatever the extension for this file is
+        let uriSuffix = options.uriSuffix
+        if (!uriSuffix) {
+          var currentFileName = document.fsPath;
+          var currentExtension = currentFileName.split('.').pop()
+          uriSuffix = "." + currentExtension
+        }
+
+        if (isAbsolute(pageName)) {
+          pageName = removeInitialSlashes(pageName)
+          href = options.baseURL + pageName + uriSuffix
+        }
+        else {
+          href = options.relativeBaseURL + pageName + uriSuffix
+        }
+
+        // append heading if present
+        if (heading) {
+          href += "#" + heading.toLowerCase();
+        }
       }
+
       href = utils.escape(href)
 
       htmlAttrs.push(`href="${href}"`)
